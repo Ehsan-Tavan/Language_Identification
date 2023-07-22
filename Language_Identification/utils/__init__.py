84cd94fa5eb483a47a60c62eccd6b2d9076d7eb8
Ehsan-Tavan <tavan.ehsan@gmail.com>
2023-07-22 17:20:38 +0330

    [add] LABEL2LANGUAGE is added.

--- Language_Identification/utils/__init__.py
+++ Language_Identification/utils/__init__.py
@@ -1,4 +1,4 @@
 from .InputExample import InputExample
 from .helper import prepare_example, encode_labels, load_dataset
 from .lightening_trainer import Trainer
-from .constant import CLASSIFIER2OBJECT, VECTORIZER2OBJECT
+from .constant import CLASSIFIER2OBJECT, VECTORIZER2OBJECT, LABEL2LANGUAGE
--- Language_Identification/utils/constant.py
+++ Language_Identification/utils/constant.py
@@ -5,3 +5,9 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 CLASSIFIER2OBJECT = {"GaussianNB": GaussianNB(), "SVC": SVC()}
 VECTORIZER2OBJECT = {"CountVectorizer": CountVectorizer(), "TfidfVectorizer": TfidfVectorizer()}
+
+LABEL2LANGUAGE = {"ar": "arabic", "bg": "bulgarian", "de": "german", "el": "modern greek",
+                  "en": "english", "es": "spanish", "fr": "french", "hi": "hindi", "it": "italian",
+                  "ja": "japanese", "nl": "dutch", "pl": "polish", "pt": "portuguese",
+                  "ru": "russian", "sw": "swahili", "th": "thai", "tr": "turkish", "ur": "urdu",
+                  "vi": "vietnamese", "zh": "chinese"}
